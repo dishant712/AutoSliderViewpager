13188fa19ca4e75b56e819ac12c60c89989ddaee
DELL <dishantvirani1@gmail.com>
2023-02-08 10:22:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/viewpager/slideradapter.java
@@ -0,0 +1,71 @@
+package com.example.viewpager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import java.util.List;
+
+public class slideradapter extends RecyclerView.Adapter<slideradapter.SliderViewHolder> {
+
+    private List<slideritem> sliderItems;
+    private ViewPager2 viewPager2;
+
+    slideradapter(List<slideritem> sliderItems, ViewPager2 viewPager2) {
+        this.sliderItems = sliderItems;
+        this.viewPager2 = viewPager2;
+    }
+
+    @NonNull
+    @Override
+    public SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new SliderViewHolder(
+                LayoutInflater.from(parent.getContext()).inflate(
+                        R.layout.slide_item_container, parent, false
+                )
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SliderViewHolder holder, int position) {
+        holder.setImage(sliderItems.get(position));
+        if (position == sliderItems.size()- 2){
+            viewPager2.post(runnable);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return sliderItems.size();
+    }
+
+    class SliderViewHolder extends RecyclerView.ViewHolder {
+        private RoundedImageView imageView;
+
+        SliderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageslide);
+        }
+
+        void setImage(slideritem sliderItems){
+
+//use glide or picasso in case you get image from internet
+            imageView.setImageResource(sliderItems.getImage());
+        }
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            sliderItems.addAll(sliderItems);
+            notifyDataSetChanged();
+        }
+    };
+
+}
---
+++ app/src/main/java/com/example/viewpager/slideritem.java
@@ -0,0 +1,15 @@
+package com.example.viewpager;
+
+public class slideritem {
+
+    //set to String, if you want to add image url from internet
+    private int image;
+
+    slideritem(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
